b0e1d15c88aedf55605d1c337571237710cafc33
SYPIAC <Reks888@hotmail.com>
2016-07-16 21:47:21 +0300

    Distributors prioritize containers over drops

--- role.distributor.js
+++ role.distributor.js
@@ -3,13 +3,22 @@ var parent = require('bot')
 var roleDistributor = {
     run: function(creep) {
         parent.run(creep);
-        if(creep.carry.energy == 0) {
-            var drops = creep.room.find(FIND_DROPPED_ENERGY);
-            if(drops.length) {
-                if(creep.pickup(drops[0])==ERR_NOT_IN_RANGE) {
-                    creep.moveTo(drops[0], {ignoreRoads: true});//carriers move the same speed on and off road, speed up the search
-                }
-            }
+        if(creep.carry.energy < creep.carryCapacity) {
+    		var containers = creep.room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_CONTAINER }});
+        	if(containers.length) {
+        		var index = 0;
+        		while(containers[index].store[RESOURCE_ENERGY] < 50 || index<containers.length-1) { index++; }
+        		if(creep.withdraw(containers[index], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        			creep.moveTo(containers[index]);
+        		}
+        	} else {
+	            var drops = creep.room.find(FIND_DROPPED_ENERGY);
+	            if(drops.length) {
+	                if(creep.pickup(drops[0])==ERR_NOT_IN_RANGE) {
+	                    creep.moveTo(drops[0], {ignoreRoads: true});//carriers move the same speed on and off road, speed up the search
+	                }
+	            }
+	        }
         } else {
             if(creep.memory.targetID != undefined) {
                 var target = Game.getObjectById(creep.memory.targetID);
@@ -24,13 +33,15 @@ var roleDistributor = {
                 var targets = creep.room.find(FIND_STRUCTURES, {
                         filter: (structure) => {
                             return (structure.structureType == STRUCTURE_EXTENSION ||
-                                    structure.structureType == STRUCTURE_SPAWN ||
-                                    structure.structureType == STRUCTURE_STORAGE) && structure.energy < structure.energyCapacity;
+                                    structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
                         }
                 });
                 targets = _.sortBy(targets, function(x) {
                   return creep.pos.getRangeTo(x.pos);
                 });
+                if(!targets.length) {
+                	targets[0] = creep.room.storage;
+                }
                 creep.memory.targetID = targets[0].id;
             }
         }
